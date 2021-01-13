639cb7e51a99b916350b452aa56c2c3870e957e8
stefanenders <75361895+stefanenders@users.noreply.github.com>
2021-01-13 17:22:17 +0100

    cleaning dead code and some comments

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,14 +6,10 @@
   import Card, {
     Content,
     PrimaryAction,
-    Media,
-    MediaContent,
     Actions,
     ActionButtons,
-    ActionIcons,
   } from "@smui/card";
   import Button, { Label } from "@smui/button";
-  import IconButton, { Icon } from "@smui/icon-button";
   import MenuSurface, { Anchor } from "@smui/menu-surface";
   import Textfield from "@smui/textfield";
   import List, {
@@ -25,26 +21,32 @@
     SecondaryText,
     Meta,
   } from "@smui/list";
-  import Fab from "@smui/fab";
-  import HelperText from "@smui/textfield/helper-text";
   import { onMount } from "svelte";
 
   //import './menu-surface.scss';
   // import { Label, Icon } from "@smui/common";
   //-----------------------------------------------
-  import { v4 as uuidv4 } from "uuid";
+
   let sliderDialog;
   let sliderDialogStop;
   let accounts = new Map();
-  let contracts = new Map();
-  let newAccounts;
   let estimatedDuration = 1;
   let desiredkWh = 80;
   let offeredFlex = 2;
   let chargedkWh = 0;
   let flexFlow = 0;
   let selectedAvailableFlex = 0;
+  let formSurface;
+  let userName = "";
 
+  function handleErrors(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
+
+  // callback functions for communicating with webapi.py
   function getAccounts() {
     fetch("http://localhost:8000/getAccounts")
       .then((d) => d.json())
@@ -55,26 +57,17 @@
           accounts.set(key, d[key]);
         }
         console.log(accounts);
-        // TODO merge existing Accounts into Map with fake names
-      }); // merge into existing accounts (we only get addresses.. so kinda useless)
-  }
-
-  function handleErrors(response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response;
+      });
   }
 
   let inCharging = [];
   function getInCharging() {
     fetch("http://localhost:8000/inCharging")
       .then((d) => d.json())
-      .then((d) => (inCharging = d)); // make safer
+      .then((d) => (inCharging = d));
   }
 
   function getBalance(userId) {
-    // perhaps buffer the values
     fetch(`http://localhost:8000/balance/${userId}`)
       .then((d) => d.json())
       .then((d) => {
@@ -87,7 +80,6 @@
   }
 
   function createAccount(userId) {
-    // const userId = uuidv4();
     console.log("creating account");
     fetch(`http://localhost:8000/createAccount/${userId}`)
       .then(handleErrors)
@@ -122,6 +114,7 @@
       .then((d) => console.log(d));
   }
 
+  // charger IDs
   let knownChargers = [
     "AG-1F01",
     "AG-1F02",
@@ -179,17 +172,6 @@
   ];
 
   //-----------------------------------------------
-  let formSurface;
-  let userName = "";
-  let desiredFlex = "";
-  let superText = "";
-  let rand = 0;
-
-  function getRand() {
-    fetch("http://localhost:8000/rand")
-      .then((d) => d.text())
-      .then((d) => (rand = d));
-  }
 
   let clicked = 0;
 
@@ -198,84 +180,24 @@
     getAccounts();
   });
 
-  function weiToEth(wei) {
-    console.log(wei, wei / 1e18);
-    return wei / 1e18;
-  }
-  // let accounts = [
-  //   { userId: "123ing", name: "something else", flex: 100 },
-  //   { userId: "some1245thing", name: "something else", flex: 100 },
-  //   { userId: "some33thing", name: "something else", flex: 100 },
-  //   { userId: "some32354thing", name: "something else", flex: 100 },
-  //   { userId: "somawfsething", name: "something else", flex: 100 },
-  //   { userId: "somdfsething", name: "something else", flex: 100 },
-  // ];
-
-  // function addAccount() {
-  //   if (input) {
-  //     accounts = [
-  //       ...accounts,
-  //       {
-  //         text: input,
-  //         id: Math.random().toString(36).substr(2, 9),
-  //       },
-  //     ];
-  //   }
-  //   input = "";
-  // }
-
-  // function removeAccount(id) {
-  //   const index = accounts.findIndex((todo) => todo.id === id);
-  //   accounts.splice(index, 1);
-  //   accounts = accounts;
-  // }
-
-  // let activeAccount;
-
-  // let chargings = [
-  //   { id: "J---aiyznGQ", name: "Keyboard Cat" },
-  //   { id: "z_AbfPXTKms", name: "Maru" },
-  //   { id: "OUtn3pvWmpg", name: "Henri The Existential Cat" },
-  //   { id: "J---ai1yznGQ", name: "Keyboar23d Cat" },
-  //   { id: "z_Ab1fPXTKms", name: "M131aru" },
-  //   { id: "OUtn31pvWmpg", name: "11Henri The Existential Cat" },
-  // ];
 
   let selectedAccountId = "Stephen Hawking";
-  // This value is updated when the component is initialized, based on the
-  // selected Item's `selected` prop.
   let selectionIndex = null;
   let selectedChargerId;
-  let addCharging = () => {};
 </script>
 
-<!-- </section> -->
+<!-- CSS section -->
 <style>
   :global(html) {
     margin:0;
     height: 100%;
     font-family: Arial, Helvetica, sans-serif;
-    /* padding: 0; */
   }
   :global(body) {
     margin:0;
     min-height: 100vh;
     font-family: Arial, Helvetica, sans-serif;
-    /* padding: 0; */
   }
-  /* .card-container {
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 500px;
-    min-width: 380px;
-    background-color: #f8f822;
-    margin-right: 20px;
-    margin-bottom: 20px;
-  } */
-  /* .card-container.short {
-    min-height: 200px;
-  } */
   * :global(.card-media-16x9) {
     background-image: url(https://via.placeholder.com/320x180.png?text=16x9);
   }
@@ -286,17 +208,12 @@
   .content-row {
     display: flex;
   }
-  /* 
-  .content-column {
-    flex: 50%;
-  } */
 
   * :global(.mdc-select) {
     width: 100%;
     margin-bottom: 5px;
   }
 
-  /* Responsive layout - makes a one column layout instead of a two-column layout */
   @media (max-width: 1280px) {
     .content-row {
       flex-direction: column;
@@ -343,10 +260,8 @@
   }
 </style>
 
-<!-- <section style="--tw-bg-opacity: 1;
-background-color: rgba(31, 41, 55, var(--tw-bg-opacity));"> -->
+<!-- HTML section -->
 <div class="header">
-  <!-- <img style="width: 150px; height: 150px;" src="logo.png" alt="logo" /> -->
   <h2 style="margin-left: 18px;">Charging Processes</h2>
   <div class="menu-bar">
     <Meta class="material-icons" style="font-size:40px; cursor:pointer" on:click={getInCharging}>refresh</Meta>
@@ -457,7 +372,6 @@ background-color: rgba(31, 41, 55, var(--tw-bg-opacity));"> -->
               step={1}
               discrete
               displayMarkers />
-            <!-- TODO must check for maxium available flex for that account -->
             <span slot="label">Flex to Boost</span>
           </FormField>
         </div>
@@ -467,7 +381,6 @@ background-color: rgba(31, 41, 55, var(--tw-bg-opacity));"> -->
           action="accept"
           on:click={() => {
             startCharging(selectedAccountId, selectedChargerId, estimatedDuration, desiredkWh, offeredFlex);
-            // contracts.set(userId, { userId, selectedChargerId, estimatedDuration, desiredkWh, offeredFlex});
             getInCharging();
             sliderDialog.close();
           }}>
@@ -490,12 +403,6 @@ background-color: rgba(31, 41, 55, var(--tw-bg-opacity));"> -->
               Charging Process No.
               {i}
             </h2>
-            <!-- <h3
-                class="mdc-typography--subtitle2"
-                style="margin: 0 0 10px; color: #888;">
-                Contract ID:
-                {chargingProcess.userID}
-              </h3> -->
             <table style="width:100%">
               <thead>
                 <tr>
@@ -538,7 +445,6 @@ background-color: rgba(31, 41, 55, var(--tw-bg-opacity));"> -->
                   selectedAvailableFlex = chargingProcess.availableFlex / 1e18;
                   sliderDialogStop.open();
                 }}>
-                <!-- <Label>Stop Charging</Label> -->
                 <Meta style="font-size: 40px;" class="material-icons">
                   power_off
                 </Meta>
